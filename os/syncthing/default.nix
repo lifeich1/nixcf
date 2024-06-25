ff95c11177f0730bdda14a61e3b8d6fe4e3b99e2
lifeich1 <lifeich0@gmail.com>
2024-06-26 04:06:13 +0800

    :wrench: syncthing: dev gtr7,matepad

--- os/syncthing/default.nix
+++ os/syncthing/default.nix
@@ -31,6 +31,15 @@ in
               id = "default";
               label = "default";
             };
+            "${pubdir}/ala-an70/default" = {
+              id = "ggt3o-3c0ib";
+              label = "ala-an70/default";
+            };
+          };
+          devices = {
+            gtr7.id = "DYFRCJJ-7WNW3GF-XHTLMH3-2UBZ2TR-6KGAAAD-APLJK6F-GCVZQEJ-OWNC7QQ";
+            matepad23.id = "VQKKXIY-JESAFSW-TE5MXQF-ND3H7IR-FKL357O-ZWNYPTP-O5OSKUC-Y4XK2AU";
+            ala_an70.id = "6EBVCNG-JWOCLAQ-QWLUM73-4E3C2GY-FGDVWP3-P563Q3D-X62YMIN-5FILAA5";
           };
         };
       };
