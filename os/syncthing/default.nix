97ec03a56a95e57037782f04c4af405eb00e762f
lifeich1 <lifeich0@gmail.com>
2024-06-30 01:59:42 +0800

    :wrench: add syncthing dirs

--- os/syncthing/default.nix
+++ os/syncthing/default.nix
@@ -30,26 +30,60 @@ in
             "${pubdir}/default" = {
               id = "default";
               label = "default";
+              devices = [
+                "gtr7"
+                "xps13_9360"
+                "matepad23"
+              ];
+            };
+            "${pubdir}/drawings" = {
+              id = "ptwgc-xwsas";
+              label = "drawings";
+              devices = [
+                "gtr7"
+                "xps13_9360"
+                "matepad23"
+              ];
             };
             "${pubdir}/priv-films" = {
               id = "vzezm-upy2v";
               label = "priv-films";
+              devices = [
+                "gtr7"
+                "xps13_9360"
+                "matepad23"
+                "ala_an70"
+              ];
             };
             "${pubdir}/priv-fast" = {
               id = "skc9k-3qpf5";
               label = "priv-fast";
+              devices = [
+                "gtr7"
+                "xps13_9360"
+                "matepad23"
+                "ala_an70"
+              ];
             };
             "${pubdir}/ala-an70/default" = {
               id = "ggt3o-3c0ib";
               label = "ala-an70/default";
+              devices = [ "ala_an70" ];
             };
             "${pubdir}/ala-an70/pics" = {
               id = "mjwxp-9joch";
               label = "ala-an70/pics";
+              devices = [ "ala_an70" ];
             };
             "${pubdir}/ala-an70/camera" = {
               id = "ala-an70_pam5-照片";
-              label = "ala-an70/pics";
+              label = "ala-an70/camera";
+              devices = [ "ala_an70" ];
+            };
+            "${pubdir}/matepad23/camera" = {
+              id = "dbr-w10_qv9a-照片";
+              label = "matepad23/camera";
+              devices = [ "matepad23" ];
             };
           };
           devices = {
