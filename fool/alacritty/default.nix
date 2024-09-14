b0c089993a9278eae1d5be97fe0adc555b665186
lifeich1 <lifeich0@gmail.com>
2024-09-15 04:03:25 +0800

    :sparkles: hm: fool.alacritty.nixgl-wrapper

--- fool/alacritty/default.nix
+++ fool/alacritty/default.nix
@@ -11,49 +11,61 @@ in
       default = 15;
       description = "alacritty terminal font size";
     };
-    # TODO unimplemented
     nixgl-wrapper = mkEnableOption "command `nixgl-alacritty` & desktop entry";
   };
 
-  config = mkIf cfg.enable {
-    programs.zellij.enable = true;
+  config = mkMerge [
+    (mkIf cfg.enable {
+      programs.zellij.enable = true;
 
-    programs.alacritty = {
-      enable = true;
-      settings = {
-        window = {
-          opacity = 0.85;
-          startup_mode = "Maximized";
-          dynamic_title = false;
-        };
-        font = {
-          size = cfg.font-size;
-          normal = {
-            style = "Regular";
-            family = "MesloLGS NF";
+      programs.alacritty = {
+        enable = true;
+        settings = {
+          window = {
+            opacity = 0.85;
+            startup_mode = "Maximized";
+            dynamic_title = false;
+          };
+          font = {
+            size = cfg.font-size;
+            normal = {
+              style = "Regular";
+              family = "MesloLGS NF";
+            };
           };
         };
       };
-    };
-  };
-
-  #home.packages = lists.optional use_gl (
-  #  let
-  #    nixglil = inputs.nixgl.packages."${system}".nixGLIntel;
-  #    pack = [
-  #      nixglil
-  #      pkgs.alacritty
-  #    ];
-  #  in
-  #  pkgs.runCommand "nixgl-alacritty"
-  #    {
-  #      buildInputs = pack;
-  #      nativeBuildInputs = [ pkgs.makeWrapper ];
-  #    }
-  #    ''
-  #      mkdir -p $out/bin/
-  #      ln -s ${nixglil}/bin/nixGLIntel $out/bin/nixgl-alacritty
-  #      wrapProgram $out/bin/nixgl-alacritty --add-flags alacritty
-  #    ''
-  #);
+    })
+    (mkIf cfg.nixgl-wrapper {
+      home.packages = [
+        (
+          let
+            nixglil = inputs.nixgl.packages."${system}".nixGLIntel;
+            pack = [
+              nixglil
+              pkgs.alacritty
+            ];
+          in
+          pkgs.runCommand "nixgl-alacritty"
+            {
+              buildInputs = pack;
+              nativeBuildInputs = [ pkgs.makeWrapper ];
+            }
+            ''
+              mkdir -p $out/bin/
+              ln -s ${nixglil}/bin/nixGLIntel $out/bin/nixgl-alacritty
+              wrapProgram $out/bin/nixgl-alacritty --add-flags alacritty
+            ''
+        )
+      ];
+      xdg.desktopEntries.nixgl-alacritty = {
+        name = "nixgl Wrapped alacritty";
+        exec = "nixgl-alacritty";
+        categories = [
+          "System"
+          "TerminalEmulator"
+        ];
+      };
+    })
+  ];
 }
