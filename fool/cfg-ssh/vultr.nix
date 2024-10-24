3aa963a484d0d472100b2b12081625ba2bee49d1
lifeich1 <lifeich0@gmail.com>
2024-10-25 01:43:48 +0800

    :wrench: vultr ayu

--- fool/cfg-ssh/vultr.nix
+++ fool/cfg-ssh/vultr.nix
@@ -12,6 +12,11 @@ in
         port 22987
         ForwardAgent Yes
 
+      Host ayu
+        HostName 64.176.41.80
+        User root
+        ForwardAgent Yes
+
       # vim: filetype=sshconfig
     '';
   };
