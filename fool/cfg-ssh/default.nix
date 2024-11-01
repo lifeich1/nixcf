493a3665b27875e1c8db5c88a1aef9d5bc46bfc1
lifeich1 <lifeich0@gmail.com>
2024-11-02 04:45:49 +0800

    :recycle: prefer real sshcfg file then embed text

--- fool/cfg-ssh/default.nix
+++ fool/cfg-ssh/default.nix
@@ -9,27 +9,30 @@ in
     qcraft = mkEnableOption "bussiness workbench ssh config";
   };
 
-  imports = [
-    ./vultr.nix
-    ./qcraft.nix
-  ];
-
-  config = {
-    home.file.".ssh/config".text = ''
-      Include config.d/*
-    '';
-    home.file.".ssh/config.d/github".text =
-      let
-        inherit (config.fool.proxy) tcp_url;
-      in
-      ''
-        Host github.com
-          HostName github.com
-          ServerAliveInterval 55
-          ForwardAgent yes
-          ProxyCommand nc -X 5 -x ${tcp_url} %h %p
-
-        # vim: filetype=sshconfig
+  config = mkMerge [
+    {
+      home.file.".ssh/config".text = ''
+        Include config.d/*
       '';
-  };
+      home.file.".ssh/config.d/github".text =
+        let
+          inherit (config.fool.proxy) tcp_url;
+        in
+        ''
+          Host github.com
+            HostName github.com
+            ServerAliveInterval 55
+            ForwardAgent yes
+            ProxyCommand nc -X 5 -x ${tcp_url} %h %p
+
+          # vim: filetype=sshconfig
+        '';
+    }
+    (mkIf cfg.vultr {
+      home.file.".ssh/config.d/vultr".source = ./vultr.cfg;
+    })
+    (mkIf cfg.qcraft {
+      home.file.".ssh/config.d/qcraft".source = ./qcraft.cfg;
+    })
+  ];
 }
