a4d202931a32fad215cdb94aaab62fc34433fc73
lifeich1 <lifeich0@gmail.com>
2024-11-02 04:35:13 +0800

    :coffin: vps rdq0a destroyed

--- fool/cfg-ssh/vultr.nix
+++ fool/cfg-ssh/vultr.nix
@@ -6,12 +6,6 @@ in
 {
   config = mkIf cfg.vultr {
     home.file.".ssh/config.d/vultr".text = ''
-      Host meta
-        HostName 144.202.44.234
-        User rdq0a
-        port 22987
-        ForwardAgent Yes
-
       Host ayu
         HostName 64.176.41.80
         User root
