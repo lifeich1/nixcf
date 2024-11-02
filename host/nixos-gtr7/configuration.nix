7538f156657605cf9c37523cd5408febbbf63cd2
lifeich1 <lifeich0@gmail.com>
2024-11-03 02:39:39 +0800

    :bug: :sparkles: gtr7 osMod

---
+++ host/nixos-gtr7/configuration.nix
@@ -0,0 +1,55 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # for pi4b remote deploy
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+  ## NOTE hardware above, userspace below
+
+  fool.sudo.nopass = true;
+
+  users.users.fool = {
+    isNormalUser = true;
+    # FIXME when ssh privkey setup ok
+    #hashedPasswordFile = config.age.secrets.gtr-pass.path;
+    description = "fool-gtr7";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "vboxusers"
+      "jackaudio"
+      "audio"
+    ];
+    shell = pkgs.zsh;
+  };
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "fool";
+
+  fool.collections.gtr = true;
+  fool.proxy.has-pi = true;
+  services.xray.enable = true;
+  fool.syncthing.enable = true;
+  fool.virtualbox.enable = true;
+}
