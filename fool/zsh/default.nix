d42fe3c830a9bd4eaeeccf7bda01e14f0f0fd570
lifeich1 <lifeich0@gmail.com>
2025-04-27 01:54:23 +0800

    :rotating_light: :alien: use zsh.initContent

--- fool/zsh/default.nix
+++ fool/zsh/default.nix
@@ -15,6 +15,7 @@ in
 
   imports = [
     ./skim.nix
+    ./instant-prompt.nix
   ];
 
   config = mkIf cfg.enable {
@@ -57,17 +58,7 @@ in
           "poetry"
         ];
       };
-      initExtraFirst = ''
-        fastfetch --pipe false
-
-        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-        # Initialization code that may require console input (password prompts, [y/n]
-        # confirmations, etc.) must go above this block; everything else may go below.
-        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-          source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-        fi
-      '';
-      initExtra = ''
+      initContent = ''
         source ~/.lintd/zsh/zshrc
         source ~/.lintd/zsh/p10k.zsh
       '';
