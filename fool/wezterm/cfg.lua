10d4110c6134d3e1292c4299a29a5f30f79c28e5
lifeich1 <lifeich0@gmail.com>
2025-07-25 01:39:14 +0800

    :wrench: wezterm: ui cfg

--- fool/wezterm/cfg.lua
+++ fool/wezterm/cfg.lua
@@ -1,6 +1,14 @@
 local wezterm = require('wezterm')
+local mux = wezterm.mux
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  local gui_window = window:gui_window();
+  gui_window:maximize()
+end)
 local cfg = {}
 cfg.window_background_opacity = 0.85
 cfg.kde_window_background_blur = false
 cfg.font = wezterm.font 'Fira Code Nerd Font'
 cfg.color_scheme = 'ayu'
+cfg.hide_tab_bar_if_only_one_tab = true
+cfg.tab_bar_at_bottom = true
