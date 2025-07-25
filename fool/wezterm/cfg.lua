cb3e60096d0e3dc7c1218187a233d90a8ef681df
lifeich1 <lifeich0@gmail.com>
2025-07-26 00:54:07 +0800

    :wrench: :rotating_light: lint wezterm cfg

--- fool/wezterm/cfg.lua
+++ fool/wezterm/cfg.lua
@@ -1,7 +1,8 @@
-local wezterm = require('wezterm')
-local mux = wezterm.mux
+---@diagnostic disable-next-line: undefined-global
+local wez = wezterm
+local mux = wez.mux
 
-wezterm.on('gui-startup', function(cmd)
+wez.on('gui-startup', function(cmd)
   ---@diagnostic disable-next-line: unused-local
   local tab, pane, window = mux.spawn_window(cmd or {})
   local gui_window = window:gui_window();
@@ -12,7 +13,7 @@ end)
 local Wrap = function(cfg)
   cfg.window_background_opacity = 0.85
   cfg.kde_window_background_blur = false
-  cfg.font = wezterm.font 'Fira Code Nerd Font'
+  cfg.font = wez.font 'Fira Code Nerd Font'
   cfg.color_scheme = 'ayu'
   cfg.hide_tab_bar_if_only_one_tab = true
   cfg.tab_bar_at_bottom = true
