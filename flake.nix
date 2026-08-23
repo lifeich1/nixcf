e6dabab7ad732761eb9034217a2fad4a76450fca
lifeich1 <lifeich0@gmail.com>
2026-08-23 22:35:05 +0800

    :arrow_up: lock update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git/?ref=nixos-unstable";
-    nixpkgs-stable.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git/?ref=nixos-25.11";
+    nixpkgs-stable.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git/?ref=nixos-26.05";
 
     ## manual mirrors
     home-manager = {
