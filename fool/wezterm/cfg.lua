c1cff3ed5b9254a6732429398db0cd542ab9dfe0
chengjie <chengjie@qcraft.ai>
2026-08-29 21:51:40 +0800

    :wrench: wezterm: use wayland blur option

--- fool/wezterm/cfg.lua
+++ fool/wezterm/cfg.lua
@@ -12,7 +12,7 @@ end)
 ---@diagnostic disable-next-line: unused-function, unused-local
 local Wrap = function(cfg)
   cfg.window_background_opacity = 0.85
-  cfg.kde_window_background_blur = false
+  cfg.wayland_window_background_blur = false
   cfg.font = wez.font 'Fira Code Nerd Font'
   cfg.color_scheme = 'ayu'
   cfg.hide_tab_bar_if_only_one_tab = true
